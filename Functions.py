960daaba43b8b219f20c8858a76a60abd111c07f
m18.kuk@gmail.com <m18.kuk@gmail.com>
2022-01-25 23:48:47 +0100

    heartdroid update

--- Functions.py
+++ Functions.py
@@ -5,13 +5,11 @@ Created on Tue Oct  5 21:37:29 2021
 @author: Edyta
 """
 import pandas as pd
-import shap
 import pickle
 import itertools
 import math
 import statistics
 import re
-import lime
 from tqdm import tqdm
 from sklearn.model_selection import train_test_split
 import numpy as np
@@ -28,40 +26,14 @@ from sklearn.decomposition import PCA, KernelPCA
 from sklearn.neighbors import NearestCentroid
 from sklearn.preprocessing import normalize, MinMaxScaler
 from sklearn.ensemble import IsolationForest
-from functools import partial
-from multiprocessing import Pool, cpu_count
-import collections
-import lightgbm as lgb
 from matplotlib import pyplot as plt
-import seaborn as sns
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_score
 from sklearn.model_selection import RandomizedSearchCV
 
 
-
-
-def load_dataframe(name):
-    with open ('datasets/'+str(name)+'.pickle', 'rb') as f:
-        df_raw = pickle.load(f)
-        #df_raw = df_raw[0]
-    #try:
-    #    df = convert_df(data = df_raw.drop('MATID', axis = 1), resample = False, res_ind = 1)
-    #except: 
-        df = convert_df(df_raw)
-    return df
-
-def convert_df(data, resample = False, res_ind = 2):
-    #temp_cols = data['y']
-    #data.drop(columns = 'y', inplace = True)
-    #data['y'] = temp_cols
-    if resample:
-        data = data.iloc[::res_ind, :]
-    data = data.reset_index(drop='index')
-    return data
-
-def prepare_df(data, test_size = 0.2):
-    df_features = data.columns.drop(data.columns[-2:])
+def prepare_df(data, test_size):
+    df_features = data.columns[:-1]
     num_of_df_features = len(df_features)
     class_names = data[data.columns[-1]].unique()
     X_train, X_test, y_train, y_test = train_test_split(data.drop(data.columns[-1], axis = 1), data[data.columns[-1]], test_size=test_size, random_state=0)
@@ -70,134 +42,81 @@ def prepare_df(data, test_size = 0.2):
 def optmize_booster(X_train, X_test, y_train, y_test, average, param_grid = {'eta': [0.001, 0.01, 0.2],
                 'max_depth': [10, 30, 50],
                 'alpha': [0.1, 0.4],
-                #'scale_pos_weight': [0.14],
-                 'max_delta_step': [0.5, 0.7],
-                 'tree_method': ['exact']}):
-    
-    best_score = -1e-6
-    best_params = {}
-    best_model = None
-    
-    unique, counts = np.unique(y_test, return_counts=True)
-    d = dict(zip(unique, counts))
-    bad_label = min(d, key=d.get)
-    
-    
-    # iterate through grid to find the best model
+                'max_delta_step': [0.5, 0.7],
+                'tree_method': ['exact']}):
+
     it_number = 0
     compute = 1
     for x in param_grid.values():
         compute = compute*len(x)
     print('Total iterations: ', compute)
           
-    #---------------------------------------------------------------
     clf = RandomizedSearchCV(xgboost.XGBClassifier(), param_grid, n_iter = compute, n_jobs = -1)
     model = clf.fit(X_train, y_train)
     y_pred = model.predict(X_test)
 
-    '''
-    for values in itertools.product(*param_grid.values()):
-        point = dict(zip(param_grid.keys(), values))
-        settings = {**point}
-        model = xgboost.XGBClassifier(**settings)
-        model.fit(X_train, y_train)
-        y_pred = model.predict(X_test)
-    '''
-    if len(np.unique(y_test)) == 2:
-        score = f1_score(y_test, y_pred, average="binary", pos_label=bad_label)
-        recall = recall_score(y_test, y_pred, average="binary", pos_label=bad_label)
-        precision = precision_score(y_test, y_pred, average="binary", pos_label=bad_label)
-            
-    else:
-        score = f1_score(y_test, y_pred, average=average)
-        recall = recall_score(y_test, y_pred, average=average)
-        precision = precision_score(y_test, y_pred, average=average)
-        
+    score = f1_score(y_test, y_pred, average=average)
+    recall = recall_score(y_test, y_pred, average=average)
+    precision = precision_score(y_test, y_pred, average=average)
     accuracy = accuracy_score(y_test, y_pred)
-    '''
-        if score > best_score:
-            best_score = score
-            recall = recall
-            precision = precision
-            accuracy = accuracy
-            best_params = settings
-            best_model = model
-        
-        print('Recall: ', round(recall,2), 
-              'f1: ', round(best_score,2),
-              'precision: ', round(precision,2),
-              'accuracy: ', round(accuracy,2),
-              'average: ', average)
-    '''
-    best_score = score
-    recall = recall
-    precision = precision
-    accuracy = accuracy
-    best_model = model
-    best_params = model.best_params_
     
     print('Recall: ', round(recall,2), 
-              'f1: ', round(best_score,2),
+              'f1: ', round(score,2),
               'precision: ', round(precision,2),
               'accuracy: ', round(accuracy,2),
               'average: ', average)
     
-    # print out results
-    print("Best parameters:", best_params)
-    print("Best score:", round(best_score,3))
-    return best_model, score, recall, precision, accuracy, best_params
+    return model, score, recall, precision, accuracy, model.best_params_
 
 
-def randomly_selected(data, class_names, procent_of_points):
-    temp_list = []
-    number_of_points = math.ceil(data.shape[0]*procent_of_points/100)
+def randomly_selected(data, class_names, number_of_points):
+    
+    if number_of_points > len(data.shape[0]):
+      number_of_points = data.shape[0]
+      print(f'Number of points to training was limited to the maximum size of dataframe: {number_of_points}')
     X = data.drop('y', axis = 1).to_numpy()
     y = data['y'].to_numpy()
+
+    temp_list = []
     for cluster in class_names:
-        #number_of_points = math.ceil(data[data['y'] == cluster].shape[0]*procent_of_points/100)
         X_t = data[data['y'] == cluster]
-        print((X_t.sample(n = number_of_points)).shape)
         temp_list.append(X_t.sample(n = number_of_points))
     df_ready = pd.concat(temp_list)
     df_ready = df_ready.set_index('y')
     return df_ready
 
-def tree_query(data, class_names, procent_of_points, metrics = 'minkowski'):
-    rng = np.random.RandomState(0)
-    number_of_points = math.ceil(data.shape[0]*procent_of_points/100)
-    temp_list = []
+def tree_query(data, class_names, number_of_points, metrics):
+    
+    if number_of_points > len(data.shape[0]):
+      number_of_points = data.shape[0]
+      print(f'Number of points to training was limited to the maximum size of dataframe: {number_of_points}')
+
     X = data.drop('y', axis = 1).to_numpy()
     y = data['y'].to_numpy()
-    print("y", y)
+
     clf = NearestCentroid()
     clf.fit(X, y)
     df_centroids = pd.DataFrame(clf.centroids_, columns = data.columns[:-1])
+    
+    temp_list = []
     for cluster in class_names:
-       # number_of_points = math.ceil(data[data['y'] == cluster].shape[0]*procent_of_points/100)
-        X_t = data[data['y'] == cluster].drop('y', axis = 1).values  # 10 points in 3 dimensions
+        X_t = data[data['y'] == cluster].drop('y', axis = 1).values
         tree = KDTree(X_t, leaf_size = 10, metric = metrics)  
         dist, ind = tree.query(df_centroids.iloc[cluster].values.reshape(1,-1), k=len(X_t))      
         temp_df = data[data['y'] == cluster].iloc[ind[0][-number_of_points:],:]
-        print(temp_df.shape)
         temp_list.append(temp_df)
-
     df_ready = pd.concat(temp_list)
     df_ready = df_ready.set_index('y')
-    
     return df_ready
 
 def outliers(data, class_names, cont):
     temp_list = []
-    number_of_points = math.ceil(data.shape[0]*cont)
     for cluster in class_names:
-        cluster_cont = number_of_points/(data[data['y'] == cluster].shape[0])
         random_data = data[data['y'] == cluster].drop('y', axis = 1).values
-        print("random_data", random_data)
-        clf = IsolationForest(random_state = 1, contamination= cluster_cont)
+        clf = IsolationForest(random_state = 1, contamination= cont)
         preds = clf.fit_predict(random_data)
         df_temp = pd.DataFrame(random_data[[i for i, x in enumerate(preds) if x == -1]], columns = data.columns[:-1])
         df_temp['y'] = [cluster] * len(df_temp)
-        print(df_temp.shape)
         temp_list.append(df_temp)
     df_ready = pd.concat(temp_list)
     df_ready = df_ready.set_index('y')
@@ -290,29 +209,24 @@ def df2hmr(data, filename, df_columns_names, confidence='product', numfeats = 2)
  
             f.write('xrule anchor/'+str(i)+': '+r['hmr_cond']+ ' ==>  [cluster set '+str(r['Cluster'])+']. #'+str(r['confidence'])+'\n')
 
-def anchor_exp(data, average = 'macro', test_size = 0.2, cont = 0.1, procent_of_points = 10, description_method = 'tree_query', threshold = 0.85, metrics = 'minkowski', model = None):
-    
-    original_data_with_matid = data.copy(deep = True)
-    X_train_with_matid, X_test_with_matid, y_train, y_test, class_names, num_of_df_features, df_features = prepare_df(original_data_with_matid, test_size)
-    X_train = X_train_with_matid.drop('MATID', axis = 1).values
-    X_test = X_test_with_matid.drop('MATID', axis = 1).values
+def anchor_exp(data, 
+              average = 'weighted', 
+              test_size = 0.2, 
+              cont = 0.1, 
+              number_of_points = 10, 
+              description_method = 'tree_query', 
+              threshold = 0.85, 
+              metrics = 'minkowski'):
+  
+    X_train, X_test, y_train, y_test, class_names, num_of_df_features, df_features = prepare_df(data, test_size)
 
-    data = X_train_with_matid.drop('MATID', axis = 1)
-    data['y'] = y_train
-    #data = original_data_with_matid.drop('MATID', axis = 1)
     orignal_features = list(data.columns[:-1])
     data.columns = ['F' + str(list(data.columns).index(x)+1) for x in list(data.columns)]
     data.columns = [*data.columns[:-1], 'y']
     modyfied_features = list(data.columns[:-1])
     df_features = modyfied_features
-        
-    #coef = sum(data['y'] ==1)/sum(data['y'] ==0)   
     
-    if model is None:
-        model, score, recall, precision, accuracy, best_params = optmize_booster(X_train, X_test, y_train, y_test, average)
-        with open('scores.pickle', 'wb') as f:
-            pickle.dump([score, recall, precision, accuracy],f)
-    model.fit(X_train, y_train)
+    model, score, recall, precision, accuracy, best_params = optmize_booster(X_train.values, X_test.values, y_train, y_test, average)
     predict_fn = lambda x: model.predict_proba(x)
     explainer = AnchorTabular(predict_fn, df_features)
     explainer.fit(X_train, disc_perc=[25, 50, 75])
@@ -320,41 +234,27 @@ def anchor_exp(data, average = 'macro', test_size = 0.2, cont = 0.1, procent_of_
     list_of_rules = []
     if description_method == 'tree_query':
         func = tree_query
-        additional_input = [data, class_names, procent_of_points, metrics]
+        additional_input = [data, class_names, number_of_points, metrics]
     elif description_method == 'outlier':
         func = outliers
         additional_input = [data, class_names, cont]
     elif description_method == 'random':
         func = randomly_selected
-        additional_input = [data, class_names, procent_of_points]
+        additional_input = [data, class_names, number_of_points]
         
-    print("Test size:", str(collections.Counter(y_test)).replace('Counter',""))
-    print(data)
-    print(class_names)
-    print(cont)
     df_input = func(*additional_input)
-    #print('Number of description points: ', df_input.shape[0])
-    print(df_input)
+    
     rules = []
-    class_names = np.unique(df_input.index)
+    #class_names = np.unique(df_input.index)
     rules_out = pd.DataFrame()
     for cluster in class_names:
         anchors = []
         for idx in range(len(df_input.values)):
-            #indexes = explainer.predictor(df_input.values[idx].reshape(1, -1))[0]
-            #if class_names[indexes] == cluster:
              if df_input.index[idx] == cluster:   
                 explanation = explainer.explain(df_input.values[idx], threshold=threshold)
                 exp = explanation.anchor
                 anchors.append('Anchor: %s' % (' AND '.join(exp)))
-                    #if scores:
-                    #    print("-------------")
-                    #    print('Cluster no: ', cluster)
-                    #    print('Precision: %.2f' % explanation['precision'])
-                    #    print('Coverage: %.2f' % explanation['coverage'])
-                    #    print('Rule: ', anchors)
                 rules_out = rules_out.append({'Rule': (' AND '.join(exp)), 'Precision': explanation['precision'], 'Coverage': explanation['coverage'], 'Cluster': cluster},                                                        ignore_index = True) 
-    print(rules_out.shape)
     rules_out['Rule'].replace('', np.nan, inplace=True)
     rules_out.dropna(subset=['Rule'], inplace=True)
     rules_out.drop_duplicates(inplace = True, ignore_index = True)
@@ -365,12 +265,10 @@ def anchor_exp(data, average = 'macro', test_size = 0.2, cont = 0.1, procent_of_
     
     rules_out_output = convert_rules(rules_out_output, orignal_features, modyfied_features)
     
-    rules_out_output.to_csv('scores/Rules_out_' + func.__name__ + '.csv', sep='\t', index = False)
+    rules_out_output.to_csv('Rules_out_' + func.__name__ + '.csv', sep='\t', index = False)
     print(f'Number of created rules: {len(rules_out_output)}')
         
-    return rules_out_output, X_test, y_test, model, len(rules_out_output), X_train_with_matid, X_test_with_matid, df_input.shape[0], best_params
-
-
+    return rules_out_output, X_test, y_test, model, len(rules_out_output), df_input.shape[0], best_params
 
 def convert_best_rules(rules):
     rules['Cov_Pre'] = rules['Coverage']*rules['Precision']
@@ -383,9 +281,6 @@ def convert_best_rules(rules):
     rules_finall.reset_index(inplace = True)
     rules_finall.drop(['index', 'Cov_Pre'], axis = 1,  inplace = True)
     return rules_finall
-
-
-
 
 def convert_rules (rules, org, mod):
     my_dict = {} 
@@ -405,107 +300,4 @@ def convert_rules (rules, org, mod):
     return rules
 
 
-
-def lime_exp(data, test_size = 0.8, cont = 0.1, max_samp = 50, number_of_points = 10, description_method = 'tree_query', metrics = 'minkowski'):
-    
-    orignal_features = list(data.columns[:-1])
-    data.columns = ['F' + str(list(data.columns).index(x)+1) for x in list(data.columns)]
-    data.columns = [*data.columns[:-1], 'y']
-    modyfied_features = list(data.columns[:-1])
-    
-    X_train, X_test, y_train, y_test, class_names, num_of_df_features, df_features = prepare_df(data)
-    
-    #model, score = optmize_booster(X_train, X_test, y_train, y_test)
-    lgb_train = lgb.Dataset(X_train, y_train)
-    lgb_eval = lgb.Dataset(X_test, y_test)
-    lgb_params = {
-    'task': 'train',
-    'boosting_type': 'goss',
-    'objective': 'binary',
-    'metric':'binary_logloss',
-    'metric': {'l2', 'auc'},
-    'num_leaves': 50,
-    'learning_rate': 0.1,
-    'feature_fraction': 0.8,
-    'bagging_fraction': 0.8,
-    'verbose': None,
-    'num_iteration':100,
-    'num_threads':7,
-    'max_depth':12,
-    'min_data_in_leaf':100,
-    'alpha':0.5}
-
-    model = lgb.train(lgb_params,lgb_train,num_boost_round=20,valid_sets=lgb_eval,early_stopping_rounds=5)
-    
-    def prob(data):
-        return np.array(list(zip(1-model.predict(data),model.predict(data))))
-
-    explainer = lime.lime_tabular.LimeTabularExplainer(X_train, feature_names=df_features, class_names=class_names, verbose=True, mode='classification')
-    if num_of_df_features > 5:
-        num_features_to_explain = 5
-    else:
-        num_features_to_explain = num_of_df_features
-
-    print('Multi dimensions')
-    print("Test size:", str(collections.Counter(y_test)).replace('Counter',""))
-    
-    if description_method == 'tree_query':
-        func = tree_query
-        additional_input = [data, class_names, number_of_points, metrics]
-    elif description_method == 'outlier':
-        func = outliers
-        additional_input = [data, class_names, cont, max_samp]
-
-    df_input = func(*additional_input)
-        
-    rule_list_lime = []
-    cert_list = []
-    cluster_label = []
-    lime_rules = pd.DataFrame()
-    for index in range(len(df_input.values)):
-        exp = explainer.explain_instance(df_input.values[index], prob, num_features=num_features_to_explain)
-        rule_list_lime.append(" AND ".join([i[0] for i in exp.as_list()]))
-        cert_list.append(abs(sum([i[1] for i in exp.as_list()])))
-        cluster_label.append(df_input.index[index])
-            
-   
-    lime_rules = pd.DataFrame({'Cluster':cluster_label, 'Coverage': 1, 'Precision': cert_list, 'Rule':rule_list_lime})
-    
-    lime_rules.drop_duplicates(inplace = True, ignore_index = True)
-    
-    df2hmr(lime_rules, 'model_lime_' + func.__name__ + '.hmr', df_columns_names = list(df_features),  numfeats = num_of_df_features)
-    
-    lime_rules = convert_rules(lime_rules, orignal_features, modyfied_features)
-    
-    print(f'Number of created rules: {len(lime_rules)}')
-    return lime_rules, X_test, y_test
-    
-def plot_conff(df, label):
-    Conf_df = df.copy(deep = True) 
-    Conf_df['heartdroid_output'] = label
-    Conf_df.rename(columns = {'y': 'orignal_lables'}, inplace=True)
-    M = Conf_df.pivot_table(index='heartdroid_output',columns='orignal_lables',values='F1',aggfunc='count').fillna(0)
-    g,axs = plt.subplots(figsize=(15,7))
-    axs = sns.heatmap(M.astype(int), annot=True, cmap='viridis',fmt='g')
-    return None
-
-
-def parallel_task(dataset, task_wrapper, threads = None):
-    if threads is None:
-        threads = cpu_count()
-    with Pool(threads) as p:
-        #print(1)
-        #dataset_split = np.array_split(dataset, cpu_count())
-        #ret_list = []
-        #print(2)
-       # 
-       # for thresh in dataset_split:
-       #     ret_list.append(p.task_wrapper(thresh))
-       ## 
-        #print(3)
-        ret_list = p.map(task_wrapper, np.array_split(dataset, threads))
-        p.close()
-    scores = pd.concat([pd.DataFrame(ret_list[x][0]) for x in range(len(ret_list))], sort=True)
-    labels = pd.concat([pd.DataFrame(ret_list[x][1]) for x in range(len(ret_list))], sort=True)
-    return scores, labels
 
